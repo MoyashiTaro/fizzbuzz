59924f438c8a126cf4e491e55bcd265aea173877
天野彰吾 <1200asmac@amanoshougonoMacBook-Pro.local>
2020-02-11 17:17:25 +0900

    pulus

---
+++ fizzbuzz_while.rb
@@ -0,0 +1,22 @@
+
+i=1
+
+while i <= 100
+
+    if i%15==0
+        puts "most-big"
+    
+    elsif i%5==0
+        puts "5の倍"
+
+    elsif i%3==0
+        puts "3の倍"
+
+    else 
+        puts i
+    end
+
+    i=i+1
+
+
+end
